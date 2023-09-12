2a89cb25f96b41a76f4482ce0e48041b91d8714e
varunsp-163 <spvarun47@gmail.com>
2023-09-12 16:12:31 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ScrollToTopButton from "./components/ScrollToTopButton";
 function App() {
   return (
     <>
-      <h1 className="text-center m-[2rem] text-xl font-bold">Shop-React-BusyFit</h1>
+      <h1 className="text-center m-[2rem] text-xl font-bold">Shop-React</h1>
 
       <Filter />
 
